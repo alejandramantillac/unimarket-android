f5f5140736ecc1908e8ba7ca8b0bfcbde013856e
alejoamu <alejoamu2@gmail.com>
2025-06-11 17:25:28 -0500

    fix: variantImages type error fixed

--- app/src/main/java/com/codeoflegends/unimarket/features/product/data/model/ProductVariant.kt
+++ app/src/main/java/com/codeoflegends/unimarket/features/product/data/model/ProductVariant.kt
@@ -6,5 +6,5 @@ data class ProductVariant(
     val id: UUID? = null,
     val name: String,
     val stock: Int,
-    val variantImages: List<R> = emptyList()
+    val variantImages: List<VariantImage> = emptyList()
 )
